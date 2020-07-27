efd666bda2ac0d3c30716254e17576f3486c3f76
chencath <chencath@hu-berlin.de>
2020-07-27 11:40:14 +0800

    Predict Nexis news

--- LSTM_Nexis.py
+++ LSTM_Nexis.py
@@ -137,7 +137,7 @@ FnameList = []
 DateList = []
 
 for queryIndex in range(len(queryList)):
-    ##print(queryList[queryIndex]["companyName"], datetime.datetime.strptime(queryList[queryIndex]["sDt"], "%Y-%m-%dT00:00:00"), datetime.datetime.strptime(queryList[queryIndex]["eDt"],"%Y-%m-%dT00:00:00"))
+    print(queryList[queryIndex]["companyName"], datetime.datetime.strptime(queryList[queryIndex]["sDt"], "%Y-%m-%dT00:00:00"), datetime.datetime.strptime(queryList[queryIndex]["eDt"],"%Y-%m-%dT00:00:00"))
     Data = mdb.ReadNewsFromDatabase(queryList[queryIndex]["companyName"], datetime.datetime.strptime(queryList[queryIndex]["sDt"], "%Y-%m-%dT%H:%M:%S"), datetime.datetime.strptime(queryList[queryIndex]["eDt"],"%Y-%m-%dT%H:%M:%S"))
     if Data != []:
         rst = LSTM_Pred(Data)
